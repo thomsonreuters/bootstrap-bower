ca48a34e43f7afd9acb579ccf8d2d1564a45707c
Dzmitry Churadze <Dzmitry.Churadze@thomsonreuters.com>
2015-09-11 13:10:28 +0300

    Fix date formatting

--- ui-bootstrap-tpls.js
+++ ui-bootstrap-tpls.js
@@ -1536,7 +1536,7 @@ function ($compile, $parse, $document, $position, dateFilter, dateParser, datepi
 
       // Outter change
       ngModel.$render = function() {
-        var date = ngModel.$viewValue ? dateFilter(ngModel.$viewValue, dateFormat) : '';
+        var date = ngModel.$modelValue ? dateFilter(ngModel.$modelValue, dateFormat) : '';
         element.val(date);
         scope.date = parseDate( ngModel.$viewValue );
       };
